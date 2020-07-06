b2867c6299617d375d384c9f15d2a1bd2ca54dbe
Thomas Cordeu <info@tcordeu.com>
2020-07-06 12:46:45 -0300

    Refactor: Use functions

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 # Update static assets.
-kubectl create configmap webapp-static-js-configmap --from-file app/build/static/js --dry-run=client -o yaml | kubectl replace -f -
-kubectl create configmap webapp-static-css-configmap --from-file app/build/static/css --dry-run=client -o yaml | kubectl replace -f -
-kubectl create configmap webapp-static-media-configmap --from-file app/build/static/media --dry-run=client -o yaml | kubectl replace -f -
-
-# Force pod resync by updating an annotation and wait.
-kubectl annotate --overwrite `kubectl get pods --selector app=webapp -o name` timestamp=`date +"%d/%m/%Y-%T"`
-sleep 5
+function update_assets {
+    for asset in js css media
+    do
+        kubectl create configmap webapp-static-$asset-configmap --from-file app/build/static/$asset --dry-run=client -o yaml | kubectl replace -f -
+    done
+}
 
 # Update public assets.
-kubectl create configmap webapp-public-configmap --from-file app/build --dry-run=client -o yaml | kubectl replace -f -
+function update_public {
+    kubectl create configmap webapp-public-configmap --from-file app/build --dry-run=client -o yaml | kubectl replace -f -
+}
 
-# Force pod resync by updating an annotation.
-kubectl annotate --overwrite `kubectl get pods --selector app=webapp -o name` timestamp=`date +"%d/%m/%Y-%T"`
+# Force pod resync by updating an annotation and wait.
+# See: https://github.com/kubernetes/website/pull/18082/files
+function sync_pod {
+    kubectl annotate --overwrite `kubectl get pods --selector app=webapp -o name` timestamp=`date +"%d/%m/%Y-%T"`
+    sleep 5
+}
+
+update_assets && sync_pod
+update_public && sync_pod
